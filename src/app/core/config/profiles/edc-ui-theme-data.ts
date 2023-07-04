83e0519bcf36a9c3cbea48eecaed953546897025
Richard Treier <richard.treier@sovity.de>
2023-07-04 16:49:58 +0200

    fix: make colours yellow

--- src/app/core/config/profiles/edc-ui-theme-data.ts
+++ src/app/core/config/profiles/edc-ui-theme-data.ts
@@ -15,7 +15,7 @@ export const MDS_THEME: EdcUiThemeConfig = {
   brandFaviconSrc: '/assets/images/mds_favicon.ico',
   brandLogoSrc: '/assets/images/mds_logo.svg',
   brandLogoStyle: 'height: 57px; margin-top: 5px; margin-left: 5px;',
-  chartColorNeutral: '#ffff18',
-  chartColorSuccess: '#9ACD32',
-  chartColorFailure: '#FFA500',
+  chartColorNeutral: '#ffff90',
+  chartColorSuccess: '#ffff18',
+  chartColorFailure: '#ffff61',
 };
